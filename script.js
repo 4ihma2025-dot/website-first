f61249a7d5de0943224b991e1fb34a2a080ae89a
4ihma2025-dot <4ihma2025@gmail.com>
2026-03-25 08:06:45 +0700

    website-first
    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function tombolKlik() {
+    var tempat = document.getElementById("tempatGambar");
+    tempat.innerHTML += '<img src="https://i.pinimg.com/originals/0e/1d/ce/0e1dce10fab9b554efeb15e832290675.gif">';
+}

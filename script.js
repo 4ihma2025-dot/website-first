3a548ef53eb80d0106f3a97c32d777ceb62808fe
4ihma2025-dot <4ihma2025@gmail.com>
2026-03-25 10:39:00 +0700

    up

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+
+// 🔥 TOMBOL → HANYA ALERT
 function tombolKlik() {
-    var tempat = document.getElementById("tempatGambar");
-    tempat.innerHTML += '<img src="https://i.pinimg.com/originals/0e/1d/ce/0e1dce10fab9b554efeb15e832290675.gif">';
+    alert("TUGAS AKHIR DIKERJAIN BOIII 🙊 coba klik gambar cy");
 }
 
+
+// 🔥 GAMBAR → GIF + DUIT
+function klikGambar() {
+    tampilkanGif();
+    keluarUang();
+}
+
+
+// 🔥 GIF
+function tampilkanGif() {
+    var tempat = document.getElementById("tempatGambar");
+
+    tempat.innerHTML += 
+    '<img src="https://i.pinimg.com/originals/0e/1d/ce/0e1dce10fab9b554efeb15e832290675.gif">';
+}
+
+
+// 💸 ANIMASI DUIT
 function keluarUang() {
     var container = document.getElementById("uangContainer");
 
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 60; i++) {
         setTimeout(() => {
+
             let uang = document.createElement("div");
             uang.classList.add("uang");
 
@@ -15,12 +35,14 @@ function keluarUang() {
 
             uang.style.left = Math.random() * 100 + "%";
 
+            uang.style.fontSize = (20 + Math.random() * 30) + "px";
+
             container.appendChild(uang);
 
             setTimeout(() => {
                 uang.remove();
             }, 3000);
 
-        }, i * 100);
+        }, i * 70);
     }
 }

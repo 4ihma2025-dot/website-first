d17ea64bd79eb5dceb82aa981644c2d52e571c96
4ihma2025-dot <4ihma2025@gmail.com>
2026-03-25 23:40:32 +0700

    FIRM UI UPDATE

--- script.js
+++ script.js
@@ -1,100 +1,17 @@
 function tombolKlik() {
-        alert("TUGAS AKHIR DIKERJAIN BOIII 🙊 UNTUK SEKARANG, RANGKUL YANG MAU SEJALAN, SELEBIHNYA HATI-HATI DIJALAN #PRINSIP #IZIIIN");
+    alert("TUGAS AKHIR DIKERJAIN BOIII 🙊");
 }
 
-function klikGambar() {
-    mainkanSuara(); //
-    tampilkanGif();
-    keluarUang();
+// POPUP
+function bukaKontak() {
+    document.getElementById("popupKontak").style.display = "flex";
 }
 
-function tampilkanGif() {
-    var tempat = document.getElementById("tempatGambar");
-
-    tempat.innerHTML += 
-    '<img src="https://i.pinimg.com/originals/0e/1d/ce/0e1dce10fab9b554efeb15e832290675.gif">';
-}
-
-function keluarUang() {
-    var container = document.getElementById("uangContainer");
-
-    for (let i = 0; i < 80; i++) {
-        setTimeout(() => {
-
-            let uang = document.createElement("div");
-            uang.className = "uang";
-
-            uang.innerHTML = ["💸","💵","🪙"][Math.floor(Math.random()*3)];
-
-            uang.style.left = Math.random() * 100 + "%";
-            uang.style.fontSize = (20 + Math.random() * 30) + "px";
-
-            container.appendChild(uang);
-
-            mainkanKoin(); // 🔥 suara tiap uang keluar
-
-            setTimeout(() => {
-                uang.remove();
-            }, 3000);
-
-        }, i * 50);
-    }
-}
-
-function mainkanSuara() {
-    var audio = document.getElementById("suaraUang");
-
-    audio.pause(); // reset dulu
-    audio.currentTime = 0;
-
-    audio.play().then(() => {
-        console.log("Audio berhasil 🔊");
-    }).catch((error) => {
-        console.log("Audio error:", error);
-    });
-}
-
-function mainkanKoin() {
-    var audio = document.getElementById("suaraKoin");
-
-    audio.currentTime = 0;
-    audio.play().catch(() => {});
+function tutupKontak() {
+    document.getElementById("popupKontak").style.display = "none";
 }
 
 // DARK MODE
 function toggleDark() {
     document.body.classList.toggle("dark");
 }
-
-// SCROLL BUTTON
-window.addEventListener("scroll", function() {
-    let btn = document.getElementById("scrollTop");
-
-    if (window.scrollY > 300) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
-});
-
-// SCROLL KE ATAS
-function scrollKeAtas() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-}
-
-// FADE ANIMATION
-window.addEventListener("scroll", function() {
-    const elements = document.querySelectorAll(".fade");
-
-    elements.forEach(el => {
-        const position = el.getBoundingClientRect().top;
-        const screen = window.innerHeight;
-
-        if (position < screen - 100) {
-            el.classList.add("show");
-        }
-    });
-});

c5bc64d27731e738cbb13e7b408eb0615a40ca35
4ihma2025-dot <4ihma2025@gmail.com>
2026-03-25 10:16:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-// 🔥 TOMBOL → munculin GIF
 function tombolKlik() {
     var tempat = document.getElementById("tempatGambar");
-
-    tempat.innerHTML += 
-    '<img src="https://i.pinimg.com/originals/0e/1d/ce/0e1dce10fab9b554efeb15e832290675.gif">';
+    tempat.innerHTML += '<img src="https://i.pinimg.com/originals/0e/1d/ce/0e1dce10fab9b554efeb15e832290675.gif">';
 }
 
-// 🔥 GAMBAR → sama fungsi
-function klikGambar() {
-    tombolKlik();
-}
-
-// 💸 ANIMASI DUIT
 function keluarUang() {
     var container = document.getElementById("uangContainer");
 
     for (let i = 0; i < 50; i++) {
         setTimeout(() => {
-
             let uang = document.createElement("div");
             uang.classList.add("uang");
 
-            // random emoji
             uang.innerHTML = ["💸","💵","🪙"][Math.floor(Math.random()*3)];
 
-            // posisi random
             uang.style.left = Math.random() * 100 + "%";
-
-            // ukuran random 🔥
-            uang.style.fontSize = (20 + Math.random() * 30) + "px";
 
             container.appendChild(uang);
 
@@ -36,6 +21,6 @@ function keluarUang() {
                 uang.remove();
             }, 3000);
 
-        }, i * 80);
+        }, i * 100);
     }
 }

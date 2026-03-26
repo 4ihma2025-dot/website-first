abbb1305cd9769b9a875e8afdf7a7d68161101ae
4ihma2025-dot <4ihma2025@gmail.com>
2026-03-26 11:28:02 +0700

    SPA

--- script.js
+++ script.js
@@ -1,57 +1,41 @@
-function tombolKlik() {
-    alert("TUGAS AKHIR DIKERJAIN BOIII 🙊");
+// LOAD NAVBAR
+fetch("navbar.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    });
+
+
+// SPA LOAD PAGE
+function loadPage(page) {
+    const app = document.getElementById("app");
+
+    // fade out
+    app.style.opacity = 0;
+
+    setTimeout(() => {
+        fetch(page)
+            .then(res => res.text())
+            .then(data => {
+                app.innerHTML = data;
+                app.style.opacity = 1;
+
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth"
+                });
+            });
+    }, 200);
 }
 
-// POPUP
-function bukaKontak() {
-    document.getElementById("popupKontak").style.display = "flex";
-}
 
-function tutupKontak() {
-    document.getElementById("popupKontak").style.display = "none";
-}
+// LOAD DEFAULT PAGE
+window.onload = function() {
+    loadPage("home.html");
+};
+
 
 // DARK MODE
 function toggleDark() {
     document.body.classList.toggle("dark");
 }
-
-function kirimEmail() {
-
-    let nama = document.getElementById("nama").value;
-    let email = document.getElementById("email").value;
-    let pesan = document.getElementById("pesan").value;
-
-    emailjs.send("service_kiwb7s5", "template_ahgmi1p", {
-        nama: nama,
-        email: email,
-        pesan: pesan
-    }).then(function(response) {
-        alert("Email berhasil dikirim 🚀");
-    }, function(error) {
-        alert("Gagal mengirim ❌");
-    });
-}
-// LOAD NAVBAR OTOMATIS
-fetch("navbar.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("navbar").innerHTML = data;
-    });
-
-    let links = document.querySelectorAll(".navbar a");
-
-links.forEach(link => {
-    if (link.href === window.location.href) {
-        link.classList.add("active");
-    }
-});
-
-fetch("navbar.html")
-    .then(res => res.text())
-    .then(data => {
-        document.getElementById("navbar").innerHTML = data;
-    })
-    .catch(err => {
-        console.error("Navbar gagal load:", err);
-    });

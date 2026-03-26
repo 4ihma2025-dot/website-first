b8f20cec4172d9350f898643f9d6fc2d842066b0
4ihma2025-dot <4ihma2025@gmail.com>
2026-03-26 10:58:23 +0700

    NAV OTOMATIS

--- script.js
+++ script.js
@@ -32,3 +32,17 @@ function kirimEmail() {
         alert("Gagal mengirim ❌");
     });
 }
+// LOAD NAVBAR OTOMATIS
+fetch("navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    });
+
+    let links = document.querySelectorAll(".navbar a");
+
+links.forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+    }
+});
